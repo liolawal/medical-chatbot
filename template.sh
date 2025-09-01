4a8ab3f72a02fdeae5907770c24760c397f07976
liolawal <onaopeib@gmail.com>
2025-09-01 14:19:30 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and file created successfully!."
