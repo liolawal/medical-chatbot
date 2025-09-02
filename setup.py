57612128c67e1c71b4aed2af290c80e1476692ab
liolawal <onaopeib@gmail.com>
2025-09-02 10:29:17 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(name="medical-chatbot", 
+      version="0.1",
+      author = "lawal ibrahim",
+    author_email = "onaopeib@gmail.com",    
+      packages=find_packages(),
+      install_requires=[]
+)
